aa6e50f28f4355adbe919d8204bd9b3554b60f2a
Zaidkhan7 <67749877+Zaidkhan7@users.noreply.github.com>
2021-03-07 17:05:55 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.ncu.Maven.LoanOrganisationSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
